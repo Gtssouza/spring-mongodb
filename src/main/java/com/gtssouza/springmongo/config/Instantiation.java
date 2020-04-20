6126077a9ccdbeb5a7c794ea04462f4d7278a46b
Gustavo Souza <gtssouza94@gmail.com>
2020-04-20 10:30:30 -0300

    referenciando os posts do usuario

--- src/main/java/com/gtssouza/springmongo/config/Instantiation.java
+++ src/main/java/com/gtssouza/springmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.gtssouza.springmongo.domain.Post;
 import com.gtssouza.springmongo.domain.User;
+import com.gtssouza.springmongo.dto.AuthorDTO;
 import com.gtssouza.springmongo.repository.PostRepository;
 import com.gtssouza.springmongo.repository.UserRepository;
 
@@ -35,11 +36,16 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo!",maria);
-		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz", maria );
-		
 		userRepository.saveAll(Arrays.asList(maria, alex,bob));
+		
+		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo!",new AuthorDTO(maria));
+		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz", new AuthorDTO(maria) );
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 		
 	}
 
