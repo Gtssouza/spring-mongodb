685eb74d36295fac1e3c47783b7fb68022b59a46
Gustavo Souza <gtssouza94@gmail.com>
2020-04-20 13:53:45 -0300

    Acrescentando comentarios ao post

--- src/main/java/com/gtssouza/springmongo/config/Instantiation.java
+++ src/main/java/com/gtssouza/springmongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.gtssouza.springmongo.domain.Post;
 import com.gtssouza.springmongo.domain.User;
 import com.gtssouza.springmongo.dto.AuthorDTO;
+import com.gtssouza.springmongo.dto.CommentDTO;
 import com.gtssouza.springmongo.repository.PostRepository;
 import com.gtssouza.springmongo.repository.UserRepository;
 
@@ -41,6 +42,12 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null,sdf.parse("21/03/2018"), "Partiu viagem!", "Vou viajar para São Paulo!",new AuthorDTO(maria));
 		Post post2 = new Post(null,sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz", new AuthorDTO(maria) );
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("21/03/2018"),new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite",sdf.parse("22/03/2018"),new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um otimo dia",sdf.parse("23/03/2018"),new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
