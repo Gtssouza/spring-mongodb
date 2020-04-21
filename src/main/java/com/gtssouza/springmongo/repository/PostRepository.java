801b49a0e384570f8fb983ee4ba16ac90a6724d3
Gustavo Souza <gtssouza94@gmail.com>
2020-04-20 21:27:16 -0300

    consulta simples com @Query

--- src/main/java/com/gtssouza/springmongo/repository/PostRepository.java
+++ src/main/java/com/gtssouza/springmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.gtssouza.springmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.gtssouza.springmongo.domain.Post;
@@ -10,6 +11,10 @@ import com.gtssouza.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 	
+	@Query("{'title':{$regex: ?0, $options:'i'}}")
+	List<Post> findByTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 
 }
